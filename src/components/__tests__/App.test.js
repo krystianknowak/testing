3470b674486f3213ac04739a1e69cd011a7b7aea
Krystian <krystian.k.nowak@gmail.com>
2020-01-14 22:29:37 +0100

    enzyme + absolute paths

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,16 +1,17 @@
 import React from "react";
-import ReactDom from "react-dom";
-
-import App from "../App";
+import { shallow } from "enzyme";
+import App from "components/App";
+import CommentBox from "components/CommentBox";
+import CommentList from "components/CommentList";
 
 it("shows a comment box", () => {
-  const div = document.createElement("div");
+  const wrapped = shallow(<App />);
+  
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});
 
-  ReactDom.render(<App />, div);
-
-  // Looks inside the div and checks to see if the commentBox is in there
-  // console.log(div.innerHTML);
-  expect(div.innerHTML).toContain("Comment Box");
-
-  ReactDom.unmountComponentAtNode(div);
+it("shows a comment list", () => {
+  const wrapped = shallow(<App />);
+  
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
