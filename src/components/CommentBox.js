69efcce65c18f89d9f974a61e405077cc9c73bf4
Krystian <krystian.k.nowak@gmail.com>
2020-01-15 23:27:05 +0100

    comment box class based

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Component } from "react";
 
-export default () => {
-  return (
-    <div>
-      Comment Box
-    </div>
-  )
+class CommentBox extends Component {
+  render() {
+    return (
+      <form>
+        <h4>Add a Comment</h4>
+        <textarea />
+        <div>
+          <button>Subbmit Comment</button>
+        </div>
+      </form>
+    );
+  }
 }
+export default CommentBox;
