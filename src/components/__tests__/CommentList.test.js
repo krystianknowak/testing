f5ddee616a15964cb16a3ca67c42ec4fdebb4d0f
Krystian <krystian.k.nowak@gmail.com>
2020-01-27 22:03:23 +0100

    commentlist test initial

---
+++ src/components/__tests__/CommentList.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { mount } from "enzyme";
+import Root from "Root";
+import CommentList from "components/CommentList";
+
+let wrapped;
+
+beforeEach(() => {
+  const initialState = {
+    comments: ["Comment 1", "Comment 2"]
+  };
+
+  wrapped = mount(
+    <Root initialState={initialState}>
+      <CommentList />
+    </Root>
+  );
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
+it("creates one LI per comment", () => {
+  expect(wrapped.find("li").length).toEqual(2);
+});
