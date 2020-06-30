8a786e1b775e73cdf70efd6b1256ed4d29d17ecb
Krystian Nowak <krystian.k.nowak@gmail.com>
2020-06-30 22:17:43 +0200

    requireAuth router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Root from "Root";
 import App from "components/App";
 
 const providerApp = (
   <Root>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+    </BrowserRouter>
   </Root>
 );
 
